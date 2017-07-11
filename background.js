373564bbcfa92c310b8afd98b1c9c02aa8b1e315
sunloverz <sunloverz.kg@gmail.com>
2017-07-11 14:00:06 +0600

    Remove x-frame headers

--- background.js
+++ background.js
@@ -1,7 +1,3 @@
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
 chrome.browserAction.onClicked.addListener(function(activeTab){
   var newURL = "http://localizejs.com/editor?url=" + activeTab.url;
   chrome.tabs.create({ url: newURL });
@@ -9,14 +5,16 @@ chrome.browserAction.onClicked.addListener(function(activeTab){
 
 chrome.webRequest.onHeadersReceived.addListener(
     function (details) {
-      for (var i = 0; i < details.responseHeaders.length; ++i) {
-        if (details.responseHeaders[i].name.toLowerCase() == 'x-frame-options') {
-          details.responseHeaders.splice(i, 1);
-          return {
-            responseHeaders: details.responseHeaders
-          };
+        //chrome.extension.getBackgroundPage().console.log(details);
+        for (var i = 0; i < details.responseHeaders.length; ++i) {
+            if (details.responseHeaders[i].name.toLowerCase() == 'x-frame-options') {
+                details.responseHeaders.splice(i, 1);
+                return {
+                    responseHeaders: details.responseHeaders
+                };
+            }
         }
-      }
     }, {
-      urls: ["<all_urls>"]
+        urls: ["<all_urls>"]
     }, ["blocking", "responseHeaders"]);
+
